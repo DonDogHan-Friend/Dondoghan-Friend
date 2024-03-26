8238ea542d3b19ae5f1e584896cffdc5d30ce5ef
jechoi <jechoi1111@gmail.com>
2024-03-26 21:31:47 +0900

    feat: eslint, prettier 설정 중간 커밋

--- DondoghanFriend/.eslintrc.js
+++ DondoghanFriend/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    extends: [
+        "@react-native",
+        "prettier",
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+    ],
+    plugins: ["prettier"],
+    rules: { "prettier/prettier": ["error", { endOfLine: "auto" }] },
 };
