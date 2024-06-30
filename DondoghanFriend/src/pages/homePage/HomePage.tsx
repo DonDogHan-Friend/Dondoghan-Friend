4962de2de32b6c946b005678dd955ecdfdc8a3b6
jechoi1111 <jechoi1111@gmail.com>
2024-06-30 15:49:25 +0900

    refactor: 주석 제거

--- DondoghanFriend/src/pages/homePage/HomePage.tsx
+++ DondoghanFriend/src/pages/homePage/HomePage.tsx
@@ -3,17 +3,14 @@ import { Image, SafeAreaView, Text, View } from "react-native";
 import { SQLiteDatabase } from "react-native-sqlite-storage";
 import { useFocusEffect } from "@react-navigation/native";
 import dayjs from "dayjs";
+import { isEmpty } from "lodash";
 
-import {
-    createSetting,
-    getThresholdSetting,
-} from "@/quries/setting/setting.ts";
+import { getThresholdSetting } from "@/quries/setting/setting.ts";
 import { GetSetting, Threshold } from "@/quries/setting/type.ts";
 import { insertComma } from "@/utils/commonUtil";
 import { getDonDogHanDBConnection } from "@/utils/connectDb.ts";
 
 import "dayjs/locale/ko";
-import {isEmpty} from "lodash";
 dayjs.locale("ko");
 
 const pig = require("@/assets/gif/pig.gif");
@@ -21,15 +18,18 @@ const diet_pig = require("@/assets/gif/diet_pig.gif");
 const angry_pig = require("@/assets/gif/angry_pig.gif");
 
 const threshold = {
-    threshold_safety: {gif: pig, color: '#A9CEC2'},
-    threshold_caution: {gif: diet_pig, color: '#F9F871'},
-    threshold_warning: {gif: angry_pig, color: '#EE005F'},
-}
+    threshold_safety: { gif: pig, color: "#A9CEC2" },
+    threshold_caution: { gif: diet_pig, color: "#F9F871" },
+    threshold_warning: { gif: angry_pig, color: "#EE005F" },
+};
 
 const HomePage = () => {
     const [db, setDb] = useState<SQLiteDatabase | null>(null);
-    const [thresholdSettingData, setThresholdSettingData] = useState<Threshold>({});
-    const [thresholdKey, setThresholdKey] = useState<keyof Threshold>('threshold_safety')
+    const [thresholdSettingData, setThresholdSettingData] = useState<Threshold>(
+        {},
+    );
+    const [thresholdKey, setThresholdKey] =
+        useState<keyof Threshold>("threshold_safety");
 
     const unit = "원";
     const now = dayjs();
@@ -62,29 +62,17 @@ const HomePage = () => {
 
     useEffect(() => {
         if (isEmpty(thresholdSettingData)) return;
-        const percent = thisMonthAmount / goalAmount * 100;
-        const keys = Object.keys(thresholdSettingData) as Array<keyof Threshold>;;
+        const percent = (thisMonthAmount / goalAmount) * 100;
+        const keys = Object.keys(thresholdSettingData) as Array<
+            keyof Threshold
+        >;
         for (const key of keys) {
             if (percent <= (thresholdSettingData[key] || 100)) {
                 setThresholdKey(key);
                 break;
             }
         }
-    }, [thresholdSettingData])
-
-    // 임계치 값이 setting table에 없는 경우
-    /*const onPressButton = () => {
-        if (db) {
-            createSetting({
-                db,
-                data: [
-                    { key: "threshold_safety", value: "30" },
-                    { key: "threshold_caution", value: "60" },
-                    { key: "threshold_warning", value: "90" },
-                ],
-            });
-        }
-    };*/
+    }, [thresholdSettingData]);
 
     const amountText = (value: number, compareValue: number) => {
         const percent = (value / compareValue) * 100;
@@ -97,7 +85,9 @@ const HomePage = () => {
     };
 
     return (
-        <SafeAreaView className={`h-screen w-screen`} style={{backgroundColor: threshold[thresholdKey].color}}>
+        <SafeAreaView
+            className={`h-screen w-screen`}
+            style={{ backgroundColor: threshold[thresholdKey].color }}>
             <View className="justify-center flex-row mt-14">
                 <View className="w-3/4 rounded-2xl p-4 flex justify-center items-center">
                     <View>
@@ -113,7 +103,10 @@ const HomePage = () => {
                 </View>
             </View>
             <View className="h-80 flex-row justify-center mt-2 bg-white rounded-t-[42px]">
-                <Image className="w-3/4 h-80" source={threshold[thresholdKey].gif} />
+                <Image
+                    className="w-3/4 h-80"
+                    source={threshold[thresholdKey].gif}
+                />
             </View>
             <View className="h-auto flex-row justify-center p-2 bg-gray-200">
                 <Text className="text-base text-[#3E4756]">
@@ -129,9 +122,6 @@ const HomePage = () => {
                     <Text>지난달 사용 금액</Text>
                     {amountText(thisMonthAmount, lastMonthAmount)}
                 </View>
-                {/*<View>*/}
-                {/*    <Button title={"button"} onPress={onPressButton} />*/}
-                {/*</View>*/}
             </View>
         </SafeAreaView>
     );
